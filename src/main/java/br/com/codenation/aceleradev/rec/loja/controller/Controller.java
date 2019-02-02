abf27978bfe12b020a9485af02bb368c4b086f71
Manasses <junior.jta26@hotmail.com>
2019-02-02 14:20:33 -0300

    Menu e assinatura do controller

--- src/main/java/br/com/codenation/aceleradev/rec/loja/controller/Controller.java
+++ src/main/java/br/com/codenation/aceleradev/rec/loja/controller/Controller.java
@@ -2,4 +2,34 @@ package br.com.codenation.aceleradev.rec.loja.controller;
 
 public class Controller {
 
+	public boolean login() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void visualizarCategorias() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void visualizarProdutosDeCategoria() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void visualizarProduto() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void adicionarProdutoNoCarrinho() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void finalizarCompra() {
+		// TODO Auto-generated method stub
+
+	}
+
 }
