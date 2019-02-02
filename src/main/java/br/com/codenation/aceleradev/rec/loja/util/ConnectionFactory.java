2e4147315a2990ba79d1f234d46f3ada5a597ae9
Gustavo <gustavo.pereira.lima@gmail.com>
2019-02-02 13:46:14 -0300

    Conection Factory

---
+++ src/main/java/br/com/codenation/aceleradev/rec/loja/util/ConnectionFactory.java
@@ -0,0 +1,21 @@
+package br.com.codenation.aceleradev.rec.loja.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+    private static String url = "jdbc:mysql://db4free.net:3306/adminproj";
+    private static String username = "adminproj";
+    private static  String password = "FSFU06hq";
+
+    public Connection getConnection() {
+        try {
+            return DriverManager.getConnection(
+                    url, username, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
